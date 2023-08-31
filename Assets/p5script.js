f5ddca66010cc056504c736f16ffa6e35e5f31c6
Milad Khodabandeh <xodabandeh@gmail.com>
2023-08-31 21:39:55 +0200

    Update p5script.js

--- Assets/p5script.js
+++ Assets/p5script.js
@@ -2,48 +2,78 @@ let xoff = 0;
 let yoff = 10000;
 let x, y;
 let prevX, prevY;
+let startTime;
+let stopDrawing = false;
 
 function setup() {
     frameRate(30)
     let cnv = createCanvas(windowWidth, windowHeight);
-    cnv.parent('canvas-container');  // Make sure the canvas is where you want it.
+    cnv.parent('canvas-container');
     background(255);
     x = width / 2;
     y = height / 2;
     prevX = x;
     prevY = y;
-    windowResized();
+    startTime = millis();
+    windowResized();  // manually trigger a resize once setup is complete
 }
 
 function draw() {
-    let angle = noise(xoff) * TWO_PI * 4;
-    let len = 5;
-    let newX = x + cos(angle) * len;
-    let newY = y + sin(angle) * len;
+    // Check if more than 10 seconds have passed
+    if (!stopDrawing || (millis() - startTime) < 10000) {
+        let angle = noise(xoff) * TWO_PI * 4;
+        let len = 5;
+        let newX = x + cos(angle) * len;
+        let newY = y + sin(angle) * len;
 
-    stroke(0);
-    strokeWeight(1);
+        stroke(0);
+        strokeWeight(1);
 
-    stroke(0,0,0);
+        line(prevX, prevY, newX, newY);
 
-    line(prevX, prevY, newX, newY);
+        prevX = newX;
+        prevY = newY;
 
-    prevX = newX;
-    prevY = newY;
+        x = newX;
+        y = newY;
 
-    x = newX;
-    y = newY;
+        xoff += 0.01;
+        yoff += 0.01;
+    }
 
-    xoff += 0.01;
-    yoff += 0.01;
-
+    // If it exits the screen, pick a random edge to re-enter
     if (x > width || x < 0 || y > height || y < 0) {
-        x = width / 2;
-        y = height / 2;
+        let edge = int(random(4));
+
+        switch(edge) {
+            case 0:  // top edge
+                x = random(width);
+                y = 0;
+                break;
+            case 1:  // bottom edge
+                x = random(width);
+                y = height;
+                break;
+            case 2:  // left edge
+                x = 0;
+                y = random(height);
+                break;
+            case 3:  // right edge
+                x = width;
+                y = random(height);
+                break;
+        }
+
         prevX = x;
         prevY = y;
+
+        // Stop drawing if 10 seconds or more have passed
+        if ((millis() - startTime) >= 30000) {
+            stopDrawing = true;
+        }
     }
 }
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
