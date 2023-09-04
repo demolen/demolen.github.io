a18016709023525913fe7dea6322e65886a3fb92
Milad Khodabandeh <xodabandeh@gmail.com>
2023-09-04 03:02:10 +0200

    more professional text and icon change

--- Assets/Language.js
+++ Assets/Language.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Initial content in German
+    // German content
     const germanContent = {
-        "IT": "Wir sind Ihr vertrauenswürdiger Partner für die Planung und Implementierung von IT- und Netzwerklösungen, die genau auf Ihre Bedürfnisse zugeschnitten sind. Wir kümmern uns um alles, von Servern und Arbeitsstationen bis hin zu DNS und E-Mail, damit Sie sich auf Ihr Kerngeschäft konzentrieren können.",
-        "Automatisierung": "Wir verstehen, dass jede Organisation einzigartig ist. Deshalb entwickeln wir maßgeschneiderte Automatisierungsskripte und Tools, die speziell auf Ihre betrieblichen Herausforderungen und Anforderungen abgestimmt sind.",
-        "Cloud": "Nutzen Sie die Flexibilität und Leistungsfähigkeit der Cloud mit unseren individuellen Lösungen für Microsoft Azure und Google Cloud. Wir begleiten Sie sicher und effizient in die Zukunft der Technologie.",
-        "Workflow": "Wir setzen nicht einfach nur Technologie ein; wir entwickeln proaktive Strategien zur Optimierung Ihres internen Workflows. Unser Ziel ist es, Ihren organisatorischen Aufwand zu minimieren und Ihre Effizienz zu steigern.",
-        "KI": "Wir sind stolz darauf, KI-Technologien einzusetzen, um intelligente und effiziente Lösungen zu entwickeln, die Ihre Geschäftsprozesse revolutionieren. Mit uns sind Sie immer einen Schritt voraus.",
-        "Design": "Lieben Sie den modernen und ansprechenden Stil dieser Website? Das ist kein Zufall! Wir sind Pioniere im Bereich des organischen und futuristischen Designs und bieten Ihnen innovative Möglichkeiten, Ihre Marke eindrucksvoll darzustellen.",
+        "IT": "Als Ihr spezialisiertes Unternehmen für IT- und Netzwerklösungen biete ich Ihnen präzise auf Ihre Anforderungen abgestimmte Dienstleistungen an. Von Serverinfrastruktur und Arbeitsstationen bis hin zu DNS und E-Mail-Management, meine Expertise ermöglicht es Ihnen, sich auf Ihre Kernkompetenzen zu fokussieren.",
+        "Automatisierung": "Ich erkenne die Einzigartigkeit jeder Organisation und entwickle daher individuelle Automatisierungslösungen, die Ihre betrieblichen Herausforderungen effizient adressieren.",
+        "Cloud": "Die Flexibilität und Leistungsfähigkeit der Cloud-Technologie wird durch meine maßgeschneiderten Lösungen für Microsoft Azure und Google Cloud optimal für Sie nutzbar gemacht. Ich unterstütze Sie dabei, zukunftssichere Technologiestrategien zu implementieren.",
+        "Workflow": "Technologie ist nur ein Instrument, und ich verwende es strategisch zur Verbesserung Ihrer internen Arbeitsprozesse. Mein Ziel ist die Reduzierung Ihres organisatorischen Aufwands und die Maximierung Ihrer Effizienz.",
+        "KI": "Die Implementierung von KI-Technologien ermöglicht es mir, intelligente und effiziente Lösungen zu schaffen, die Ihre Geschäftsprozesse transformieren. So bleiben Sie immer einen Schritt voraus.",
+        "Design": "Ist Ihnen der ansprechende Stil dieser Webseite aufgefallen? Das ist das Ergebnis meiner Expertise im Bereich modernen und futuristischen Designs. Ich biete Ihnen vielfältige Möglichkeiten, Ihre Marke effektvoll zu präsentieren.",
         "menu_IT": "IT",
         "menu_Automatisierung": "Automatisierung",
         "menu_Cloud": "Cloud Lösungen",
@@ -15,14 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
         "menu_Design": "Design"
     };
 
-    // English translations
+// English translations
     const englishContent = {
-        "IT": "We are your trusted partner for planning and implementing IT and network solutions tailored specifically to your needs. We handle everything from servers and workstations to DNS and email so you can focus on your core business.",
-        "Automatisierung": "We understand that each organization is unique. Therefore, we develop custom automation scripts and tools tailored to your operational challenges and requirements.",
-        "Cloud": "Leverage the flexibility and power of the cloud with our bespoke solutions for Microsoft Azure and Google Cloud. We securely and efficiently guide you into the future of technology.",
-        "Workflow": "We don’t just implement technology; we proactively develop strategies for optimizing your internal workflow. Our aim is to minimize your organizational effort and boost your efficiency.",
-        "KI": "We take pride in implementing AI technologies to develop smart and efficient solutions that revolutionize your business processes. With us, you are always one step ahead.",
-        "Design": "Do you love the modern and appealing style of this website? That's no coincidence! We are pioneers in the field of organic and futuristic design, offering you innovative ways to dramatically present your brand.",
+        "IT": "As your specialized company for IT and network solutions, I offer services tailored precisely to your needs. From server infrastructure and workstations to DNS and email management, my expertise allows you to focus on your core competencies.",
+        "Automatisierung": "I recognize the uniqueness of each organization and therefore develop individual automation solutions that efficiently address your operational challenges.",
+        "Cloud": "The flexibility and performance of cloud technology are optimally utilized for you through my customized solutions for Microsoft Azure and Google Cloud. I support you in implementing future-proof technology strategies.",
+        "Workflow": "Technology is merely a tool, and I use it strategically to improve your internal work processes. My aim is to reduce your organizational effort and maximize your efficiency.",
+        "KI": "The implementation of AI technologies allows me to create intelligent and efficient solutions that transform your business processes. This keeps you always one step ahead.",
+        "Design": "Did you notice the appealing style of this website? That is the result of my expertise in modern and futuristic design. I offer you a variety of ways to effectively present your brand.",
         "menu_IT": "IT",
         "menu_Automatisierung": "Automation",
         "menu_Cloud": "Cloud Solutions",
